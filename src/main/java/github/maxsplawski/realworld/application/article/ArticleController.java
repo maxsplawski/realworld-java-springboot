5fc238800ae7af438ab3d232ca6e7dcc89f6064b
Maksymilian Spławski <mniamek9@gmail.com>
2024-05-20 10:10:01 +0200

    feat: add getArticle method

--- src/main/java/github/maxsplawski/realworld/application/article/ArticleController.java
+++ src/main/java/github/maxsplawski/realworld/application/article/ArticleController.java
@@ -3,21 +3,25 @@ package github.maxsplawski.realworld.application.article;
 import github.maxsplawski.realworld.application.article.dto.SaveArticle;
 import github.maxsplawski.realworld.application.article.service.ArticleService;
 import github.maxsplawski.realworld.domain.article.Article;
+import github.maxsplawski.realworld.domain.article.ArticleRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/articles")
 public class ArticleController {
     private final ArticleService articleService;
+    private final ArticleRepository articleRepository;
 
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, ArticleRepository articleRepository) {
         this.articleService = articleService;
+        this.articleRepository = articleRepository;
     }
 
     @PostMapping("")
@@ -27,5 +31,25 @@ public class ArticleController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(createdArticle);
+    }
+
+    @GetMapping("/{slug}")
+    public ResponseEntity<Map<String, Article>> getArticle(@PathVariable String slug) {
+        Optional<Article> optionalArticle = articleRepository.findBySlug(slug);
+
+        if (optionalArticle.isPresent()) {
+            Article article = optionalArticle.get();
+
+            Map<String, Article> responseBody = new HashMap<>();
+            responseBody.put("article", article);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(responseBody);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
     }
 }
--- src/main/java/github/maxsplawski/realworld/domain/article/ArticleRepository.java
+++ src/main/java/github/maxsplawski/realworld/domain/article/ArticleRepository.java
@@ -2,5 +2,8 @@ package github.maxsplawski.realworld.domain.article;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    Optional<Article> findBySlug(String slug);
 }

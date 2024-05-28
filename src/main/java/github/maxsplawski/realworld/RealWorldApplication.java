befb2eff44ced261ec27582ad002ddbc471fe19c
Maksymilian Spławski <mniamek9@gmail.com>
2024-05-28 21:06:04 +0200

    test: add ArticleControllerTest

--- src/main/java/github/maxsplawski/realworld/RealWorldApplication.java
+++ src/main/java/github/maxsplawski/realworld/RealWorldApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"github.maxsplawski.realworld.*"})
-@EnableJpaRepositories(basePackages = {"github.maxsplawski.realworld.domain.*"})
 public class RealWorldApplication {
 
 	public static void main(String[] args) {

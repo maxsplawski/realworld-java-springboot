48ced6fd27fdc132485e9016f633bb5ea5fa92b6
Maksymilian Spławski <mniamek9@gmail.com>
2024-06-25 20:07:57 +0200

    feat: add register method in AuthController

--- src/main/java/github/maxsplawski/realworld/application/auth/AuthController.java
+++ src/main/java/github/maxsplawski/realworld/application/auth/AuthController.java
@@ -1,33 +1,56 @@
 package github.maxsplawski.realworld.application.auth;
 
 import github.maxsplawski.realworld.application.auth.dto.LoginRequest;
-import github.maxsplawski.realworld.application.auth.service.JwtTokenService;
+import github.maxsplawski.realworld.application.auth.dto.LoginResponse;
+import github.maxsplawski.realworld.application.auth.dto.RegisterRequest;
+import github.maxsplawski.realworld.application.auth.service.AuthService;
+import github.maxsplawski.realworld.domain.user.SecurityUserDetails;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenService jwtTokenService;
+    private final AuthService authService;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtTokenService jwtTokenService) {
-        this.authenticationManager = authenticationManager;
-        this.jwtTokenService = jwtTokenService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest loginRequest) {
-        Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.getUsername(), loginRequest.getPassword());
-        Authentication authenticationResponse = this.authenticationManager.authenticate(authenticationRequest);
+    public ResponseEntity<Map<String, LoginResponse>> login(@Valid @RequestBody LoginRequest loginRequest) {
+        SecurityUserDetails userDetails = this.authService.login(loginRequest);
 
-        String token = this.jwtTokenService.generateToken(authenticationResponse);
+        Map<String, LoginResponse> responseBody = new HashMap<>();
+        responseBody.put("user", new LoginResponse(
+                userDetails.getUser().getEmail(),
+                userDetails.getToken(),
+                userDetails.getUsername(),
+                userDetails.getUser().getBio(),
+                userDetails.getUser().getImage()
+        ));
 
-        return ResponseEntity.ok().body(token);
+        return ResponseEntity.ok().body(responseBody);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, LoginResponse>> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        SecurityUserDetails userDetails = this.authService.register(registerRequest);
+
+        Map<String, LoginResponse> responseBody = new HashMap<>();
+        responseBody.put("user", new LoginResponse(
+                userDetails.getUser().getEmail(),
+                userDetails.getToken(),
+                userDetails.getUsername(),
+                userDetails.getUser().getBio(),
+                userDetails.getUser().getImage()
+        ));
+
+        return ResponseEntity.ok().body(responseBody);
     }
 }
--- src/test/java/github/maxsplawski/realworld/application/auth/AuthControllerTest.java
+++ src/test/java/github/maxsplawski/realworld/application/auth/AuthControllerTest.java
@@ -1,16 +1,24 @@
 package github.maxsplawski.realworld.application.auth;
 
-import github.maxsplawski.realworld.application.auth.service.JwtTokenService;
+import github.maxsplawski.realworld.application.auth.dto.LoginRequest;
+import github.maxsplawski.realworld.application.auth.service.AuthService;
 import github.maxsplawski.realworld.config.security.JpaUserDetailsService;
 import github.maxsplawski.realworld.config.security.SecurityConfiguration;
+import github.maxsplawski.realworld.domain.user.SecurityUserDetails;
+import github.maxsplawski.realworld.domain.user.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ContextConfiguration(classes = {AuthController.class, SecurityConfiguration.class})
@@ -24,12 +32,41 @@ class AuthControllerTest {
     private JpaUserDetailsService jpaUserDetailsService;
 
     @MockBean
-    private JwtTokenService jwtTokenService;
+    private AuthService authService;
 
     @Test
     public void returnsUnauthorizedResponseForAnUnauthenticatedUser() throws Exception {
         this.mockMvc
                 .perform(get("/api/articles"))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void returnsATokenFromLoginEndpoint() throws Exception {
+        User user = new User(
+                "john.doe@mail.com",
+                "John Doe",
+                new BCryptPasswordEncoder().encode("password"),
+                "Hello, there!",
+                "https://images.com/image",
+                "ROLE_USER"
+        );
+        SecurityUserDetails userDetails = new SecurityUserDetails(user);
+
+        when(this.authService.login(any(LoginRequest.class))).thenReturn(userDetails);
+
+        this.mockMvc
+                .perform(post("/login")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // language=json
+                        .content("""
+                                    {
+                                        "username": "John Doe",
+                                        "password": "password"
+                                    }
+                                """))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 }

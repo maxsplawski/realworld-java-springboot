2ff3175e26d5b8c5cdbb034f0a20d48b1a269588
Maksymilian Spławski <mniamek9@gmail.com>
2024-06-25 16:44:34 +0200

    test: add AuthControllerTest

--- src/main/java/github/maxsplawski/realworld/RealWorldApplication.java
+++ src/main/java/github/maxsplawski/realworld/RealWorldApplication.java
@@ -1,14 +1,11 @@
 package github.maxsplawski.realworld;
 
-import github.maxsplawski.realworld.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"github.maxsplawski.realworld.*"})
-@EnableConfigurationProperties(RsaKeyProperties.class)
 public class RealWorldApplication {
 
     public static void main(String[] args) {

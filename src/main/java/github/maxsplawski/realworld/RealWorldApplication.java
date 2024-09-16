687cefef1d4dbe9249c444de4aa496353e22c4d9
Max Spławski <mniamek9@gmail.com>
2024-09-16 16:57:34 +0200

    refactor: add formatting changes in multiple files

--- src/main/java/github/maxsplawski/realworld/RealWorldApplication.java
+++ src/main/java/github/maxsplawski/realworld/RealWorldApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"github.maxsplawski.realworld.*"})
 public class RealWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RealWorldApplication.class, args);
     }
-
 }
